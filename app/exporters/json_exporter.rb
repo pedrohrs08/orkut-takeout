54dfdbf6afea860f1070515a75914b6aeaaba165
Pedro Soares <psoares@avenuecode.com>
2016-03-04 15:27:17 -0300

    adding logic to the third exporter

--- app/exporters/json_exporter.rb
+++ app/exporters/json_exporter.rb
@@ -1,11 +1,22 @@
 class JSONExporter 
+	
+	FRIENDLY = "Friendly"
+	NOT_SO_FRIENDLY = "Not So Friendly"
+	SUPER_FRIENDLY = "Super Friendly"
+
 	def export_friends(input = [], user = {})
+		    
+		    socialPercentageNumber = get_social_percentage(input.size)
+		    socialTypeText = get_social_type(socialPercentageNumber)
+
 			json = JSONBuilder::Compiler.generate do
 		    	count input.size
 		    	
 		    	user user do |user|
 		    		name user[:name.to_s]
 		    		email user[:email.to_s]
+		            socialPercentage socialPercentageNumber.to_s + "%"
+		            socialType socialTypeText
 		    	end
 
 		    	friends input do |entry|
@@ -15,5 +26,20 @@ class JSONExporter
 
 		    end
 		    json
+	end
+
+	private 
+	def get_social_percentage(friends_count)
+		(friends_count * 100) / 15
+	end
+
+	def get_social_type(social_percentage)
+		if social_percentage > 80
+			return SUPER_FRIENDLY
+		elsif social_percentage > 30
+			return FRIENDLY
+		else
+			return NOT_SO_FRIENDLY
+		end 			
 	end
 end
